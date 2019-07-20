16bbe15811703c7a569399aac186a478dabf32f3
owen <649073762@qq.com>
2019-07-20 18:50:25 +0800

    Story2 AC 2 Update orderStatus by carId test success

--- src/test/java/com/thoughtworks/parking_lot/ParkingLotOrderTest.java
+++ src/test/java/com/thoughtworks/parking_lot/ParkingLotOrderTest.java
@@ -3,7 +3,9 @@ package com.thoughtworks.parking_lot;
 import com.thoughtworks.parking_lot.entiry.Car;
 import com.thoughtworks.parking_lot.entiry.ParkingLot;
 import com.thoughtworks.parking_lot.service.ParkingLotOrderService;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,4 +43,24 @@ public class ParkingLotOrderTest {
                 .andExpect(status().isCreated());
     }
 
+    @Test
+    public void should_return_order_when_end_the__parking_order() throws Exception {
+        //given
+        Car car =new Car();
+        car.setCarId("hk-001");
+        JSONObject jsonObject = new JSONObject(car);
+        //when
+        this.mockMvc.perform(post("/parkinglots/parkingLotOrders").content(jsonObject.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+       String result =  this.mockMvc.perform(patch("/parkinglots/parkingLotOrders").content(jsonObject.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isAccepted()).andReturn().getResponse().getContentAsString();
+
+       //then
+        JSONObject jsonObject1 =new JSONObject(result);
+        Assertions.assertEquals(0,jsonObject1.getInt("orderStatus"));
+
+    }
 }
